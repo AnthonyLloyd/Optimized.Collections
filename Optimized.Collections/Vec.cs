e240063047bcc80a93beb809742f81976ae80407
Anthony Lloyd <anthony@lloydhq.com>
2022-04-09 21:05:58 +0100

    vec all list read functions

--- Optimized.Collections/Vec.cs
+++ Optimized.Collections/Vec.cs
@@ -5,18 +5,19 @@ namespace Optimized.Collections;
 
 /// <summary>Represents a strongly typed grow only list of objects that can be accessed by index.</summary>
 /// <remarks>
-/// - Lock free for reads during modification for reference types and value types that are set atomically.<br/>
+/// - Lock free for reads during modification for reference types (and value types that set atomically).<br/>
 /// - More control of memory use and excess capacity.<br/>
+/// - Slightly better performance than <see cref="List{T}"/> in general.<br/>
 /// </remarks>
 /// <typeparam name="T">The type of elements in the list.</typeparam>
 public class Vec<T> : IReadOnlyList<T>
 {
-    static readonly T[] emptyArray = new T[0];
+    static readonly T[] s_emptyArray = new T[0];
     T[] _items;
     int _count;
 
     /// <summary>Initializes a new instance of the <see cref="Vec{T}"/> class that is empty with no initial capacity.</summary>
-    public Vec() => _items = emptyArray;
+    public Vec() => _items = s_emptyArray;
 
     /// <summary>Initializes a new instance of the <see cref="Vec{T}"/> that is empty and has the specified initial capacity.</summary>
     /// <param name="capacity">The number of elements that the new vec can initially store.</param>
@@ -38,6 +39,32 @@ public class Vec<T> : IReadOnlyList<T>
     /// <summary>Gets the number of elements contained in the <see cref="Vec{T}"/>.</summary>
     /// <returns>The number of elements contained in the <see cref="Vec{T}"/>.</returns>
     public int Count => _count;
+
+    /// <summary>Gets or sets the total number of elements the internal data structure can hold without resizing.</summary>
+    /// <returns>The number of elements that the <see cref="Vec{T}"/> can contain before resizing is required.</returns>
+    public int Capacity
+    {
+        get => _items.Length;
+        set
+        {
+            if (value != _items.Length)
+            {
+                if (value > 0)
+                {
+                    T[] newItems = new T[value];
+                    if (_count > 0)
+                    {
+                        Array.Copy(_items, newItems, _count);
+                    }
+                    _items = newItems;
+                }
+                else
+                {
+                    _items = s_emptyArray;
+                }
+            }
+        }
+    }
 
     /// <summary>Gets or sets the element at the specified index.</summary>
     /// <param name="index">The zero-based index of the element to get or set.</param>
@@ -97,8 +124,17 @@ public class Vec<T> : IReadOnlyList<T>
     /// <summary>Sets the capacity to the actual number of elements in the <see cref="Vec{T}"/>.</summary>
     public void TrimExcess()
     {
-        Array.Resize(ref _items, _count);
+        if (_count == 0)
+            _items = s_emptyArray;
+        else
+            Array.Resize(ref _items, _count);
     }
+
+    /// <summary>Determines whether an element is in the <see cref="Vec{T}"/>.</summary>
+    /// <param name="item">The object to locate in the <see cref="Vec{T}"/>. The value can be null for reference types.</param>
+    /// <returns>true if item is found in the <see cref="Vec{T}"/>; otherwise, false.</returns>
+    public bool Contains(T item)
+        => IndexOf(item) >= 0;
 
     /// <summary>Searches for the specified object and returns the zero-based index of the first occurrence within the entire <see cref="Vec{T}"/>.</summary>
     /// <param name="item">The object to locate in the <see cref="Vec{T}"/>. The value can be null for reference types.</param>
@@ -155,6 +191,214 @@ public class Vec<T> : IReadOnlyList<T>
     public int LastIndexOf(T item, int index, int count)
     {
         return Array.LastIndexOf(_items, item, index, count);
+    }
+
+    /// <summary>Converts the elements in the current <see cref="Vec{T}"/> to another type, and returns a list containing the converted elements.</summary>
+    /// <typeparam name="TOutput">The type of the elements of the target <see cref="Vec{T}"/>.</typeparam>
+    /// <param name="converter">A <see cref="Converter{T, TOutput}"/> delegate that converts each element from one type to another type.</param>
+    /// <returns>A <see cref="Vec{T}"/> of the target type containing the converted elements from the current <see cref="Vec{T}"/>.</returns>
+    public Vec<TOutput> ConvertAll<TOutput>(Converter<T, TOutput> converter)
+    {
+        var count = _count;
+        var items = _items;
+        var vec = new Vec<TOutput>(count);
+        for (int i = 0; i < count; i++)
+        {
+            vec._items[i] = converter(items[i]);
+        }
+        vec._count = count;
+        return vec;
+    }
+
+    /// <summary>Copies the entire <see cref="Vec{T}"/> to a compatible one-dimensional array, starting at the beginning of the target array.</summary>
+    /// <param name="array">The one-dimensional System.Array that is the destination of the elements copied from <see cref="Vec{T}"/>. The System.Array must have zero-based indexing.</param>
+    public void CopyTo(T[] array)
+    {
+        var count = _count;
+        Array.Copy(_items, array, count);
+    }
+
+    /// <summary>Copies the entire <see cref="Vec{T}"/> to a compatible one-dimensional array, starting at the specified index of the target array.</summary>
+    /// <param name="array">The one-dimensional array that is the destination of the elements copied from <see cref="Vec{T}"/>. The System.Array must have zero-based indexing.</param>
+    /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        var count = _count;
+        Array.Copy(_items, 0, array, arrayIndex, count);
+    }
+
+    /// <summary>Copies a range of elements from the <see cref="Vec{T}"/> to a compatible one-dimensional array, starting at the specified index of the target array.</summary>
+    /// <param name="index">The zero-based index in the source <see cref="Vec{T}"/> at which copying begins.</param>
+    /// <param name="array">The one-dimensional array that is the destination of the elements copied from <see cref="Vec{T}"/>. The System.Array must have zero-based indexing.</param>
+    /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
+    /// <param name="count">The number of elements to copy.</param>
+    public void CopyTo(int index, T[] array, int arrayIndex, int count)
+    {
+        Array.Copy(_items, index, array, arrayIndex, count);
+    }
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the first occurrence within the entire <see cref="Vec{T}"/>.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The first element that matches the conditions defined by the specified predicate, if found; otherwise, the default value for type T.</returns>
+    public T? Find(Predicate<T> match)
+    {
+        var count = _count;
+        var items = _items;
+        for (int i = 0; i < count; i++)
+        {
+            if (match(items[i]))
+            {
+                return items[i];
+            }
+        }
+        return default;
+    }
+
+    /// <summary>Retrieves all the elements that match the conditions defined by the specified predicate.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the elements to search for.</param>
+    /// <returns>A <see cref="Vec{T}"/> containing all the elements that match the conditions defined by the specified predicate, if found; otherwise, an empty <see cref="Vec{T}"/>.</returns>
+    public Vec<T> FindAll(Predicate<T> match)
+    {
+        var count = _count;
+        var items = _items;
+        var vec = new Vec<T>();
+        for (int i = 0; i < count; i++)
+        {
+            if (match(items[i]))
+            {
+                vec.Add(items[i]);
+            }
+        }
+        return vec;
+    }
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the first occurrence within the range of elements in the <see cref="Vec{T}"/> that starts at the specified index and contains the specified number of elements.</summary>
+    /// <param name="startIndex">The zero-based starting index of the search.</param>
+    /// <param name="count">The number of elements in the section to search.</param>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The zero-based index of the first occurrence of an element that matches the conditions defined by match, if found; otherwise, –1.</returns>
+    public int FindIndex(int startIndex, int count, Predicate<T> match)
+    {
+        var items = _items;
+        int endIndex = startIndex + count;
+        for (int i = startIndex; i < endIndex; i++)
+        {
+            if (match(items[i])) return i;
+        }
+        return -1;
+    }
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the first occurrence within the range of elements in the <see cref="Vec{T}"/> that extends from the specified index to the last element.</summary>
+    /// <param name="startIndex">The zero-based starting index of the search.</param>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The zero-based index of the first occurrence of an element that matches the conditions defined by match, if found; otherwise, –1.</returns>
+    public int FindIndex(int startIndex, Predicate<T> match)
+        => FindIndex(startIndex, _count - startIndex, match);
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the first occurrence within the entire <see cref="Vec{T}"/>.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The zero-based index of the first occurrence of an element that matches the conditions defined by match, if found; otherwise, –1.</returns>
+    public int FindIndex(Predicate<T> match)
+        => FindIndex(0, _count, match);
+
+    /// <summary>Determines whether the <see cref="Vec{T}"/> contains elements that match the conditions defined by the specified predicate.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the elements to search for.</param>
+    /// <returns>true if the <see cref="Vec{T}"/> contains one or more elements that match the conditions defined by the specified predicate; otherwise, false.</returns>
+    public bool Exists(Predicate<T> match)
+        => FindIndex(match) != -1;
+
+    /// <summary>Determines whether the <see cref="Vec{T}"/> contains elements that match the conditions defined by the specified predicate.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the elements to search for.</param>
+    /// <returns>true if the <see cref="Vec{T}"/> contains one or more elements that match the conditions defined by the specified predicate; otherwise, false.</returns>
+    public T? FindLast(Predicate<T> match)
+    {
+        var count = _count;
+        var items = _items;
+        for (int i = count - 1; i >= 0; i--)
+        {
+            if (match(items[i]))
+            {
+                return items[i];
+            }
+        }
+        return default;
+    }
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the last occurrence within the range of elements in the <see cref="Vec{T}"/> that contains the specified number of elements and ends at the specified index.</summary>
+    /// <param name="startIndex">The zero-based starting index of the backward search.</param>
+    /// <param name="count">The number of elements in the section to search.</param>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The zero-based index of the last occurrence of an element that matches the conditions defined by match, if found; otherwise, –1.</returns>
+    public int FindLastIndex(int startIndex, int count, Predicate<T> match)
+    {
+        if (count == 0) return -1;
+        var items = _items;
+        int endIndex = startIndex - count;
+        for (int i = startIndex; i > endIndex; i--)
+        {
+            if (match(items[i]))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the last occurrence within the entire <see cref="Vec{T}"/>.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The zero-based index of the last occurrence of an element that matches the conditions defined by match, if found; otherwise, –1.</returns>
+    public int FindLastIndex(Predicate<T> match)
+    {
+        var count = _count;
+        return FindLastIndex(count - 1, count, match);
+    }
+
+    /// <summary>Searches for an element that matches the conditions defined by the specified predicate, and returns the zero-based index of the last occurrence within the range of elements in the <see cref="Vec{T}"/> that extends from the first element to the specified index.</summary>
+    /// <param name="startIndex">The zero-based starting index of the backward search.</param>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions of the element to search for.</param>
+    /// <returns>The zero-based index of the last occurrence of an element that matches the conditions defined by match, if found; otherwise, –1.</returns>
+    public int FindLastIndex(int startIndex, Predicate<T> match)
+        => FindLastIndex(startIndex, startIndex + 1, match);
+
+    /// <summary>Performs the specified action on each element of the <see cref="Vec{T}"/>.</summary>
+    /// <param name="action">The System.Action`1 delegate to perform on each element of the <see cref="Vec{T}"/>.</param>
+    public void ForEach(Action<T> action)
+    {
+        var count = _count;
+        var items = _items;
+        for (int i = 0; i < count; i++)
+        {
+            action(items[i]);
+        }
+    }
+
+    /// <summary>Determines whether every element in the <see cref="Vec{T}"/> matches the conditions defined by the specified predicate.</summary>
+    /// <param name="match">The <see cref="Predicate{T}"/> delegate that defines the conditions to check against the elements.</param>
+    /// <returns>true if every element in the <see cref="Vec{T}"/> matches the conditions defined by the specified predicate; otherwise, false. If the list has no elements, the return value is true.</returns>
+    public bool TrueForAll(Predicate<T> match)
+    {
+        var count = _count;
+        var items = _items;
+        for (int i = 0; i < count; i++)
+        {
+            if (!match(items[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// <summary>Creates a shallow copy of a range of elements in the source <see cref="Vec{T}"/>.</summary>
+    /// <param name="index">The zero-based <see cref="Vec{T}"/> index at which the range starts.</param>
+    /// <param name="count">The number of elements in the range.</param>
+    /// <returns>A shallow copy of a range of elements in the source <see cref="Vec{T}"/>.</returns>
+    public Vec<T> GetRange(int index, int count)
+    {
+        var vec = new Vec<T>(count);
+        Array.Copy(_items, index, vec._items, 0, count);
+        vec._count = count;
+        return vec;
     }
 
     /// <summary>Copies the elements of the <see cref="Vec{T}"/> to a new array.</summary>
