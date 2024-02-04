f3b5892c702b6e8ebe98987dadc68c0f2155aaf7
Anthony Lloyd <anthony@lloydhq.com>
2024-02-04 19:37:09 +0000

    add SieveLruCache test

--- Optimized.Collections/SieveLruCache.cs
+++ Optimized.Collections/SieveLruCache.cs
@@ -1,10 +1,11 @@
 ﻿namespace Optimized.Collections;
 
+using System.Collections;
 using System.Collections.Concurrent;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-public class SieveLruCache<K, V>(int capacity) where K : notnull
+public class SieveLruCache<K, V>(int capacity) : IEnumerable<KeyValuePair<K, V>> where K : notnull
 {
     class Node(K key, V value)
     {
@@ -16,7 +17,7 @@ public class SieveLruCache<K, V>(int capacity) where K : notnull
     }
 
     private readonly ConcurrentDictionary<K, Node> _dictionary = [];
-    private readonly ConcurrentDictionary<K, TaskCompletionSource> _taskCompletionSources = [];
+    private readonly ConcurrentDictionary<K, TaskCompletionSource<V>> _taskCompletionSources = [];
     private Node? head;
     private Node? tail;
     private Node? hand;
@@ -58,46 +59,47 @@ public class SieveLruCache<K, V>(int capacity) where K : notnull
             tail = node.Prev;
     }
 
-    public async Task<V> GetOrAddAtomicAsync(K key, Func<K, Task<V>> factory)
+    public async Task<V> GetAsync(K key, Func<K, Task<V>> factory)
     {
-        while (true)
+        if (_dictionary.TryGetValue(key, out var node))
         {
-            if (_dictionary.TryGetValue(key, out var node))
+            node.Visited = true;
+            return node.Value;
+        }
+        var myTCS = new TaskCompletionSource<V>();
+        var tcs = _taskCompletionSources.GetOrAdd(key, myTCS);
+        if (tcs != myTCS) return await tcs.Task;
+        try
+        {
+            if (_dictionary.TryGetValue(key, out node))
             {
                 node.Visited = true;
                 return node.Value;
             }
-            var myTCS = new TaskCompletionSource();
-            var tcs = _taskCompletionSources.GetOrAdd(key, myTCS);
-            if (myTCS == tcs)
+            var value = await factory(key);
+            node = new Node(key, value);
+            lock (_dictionary)
             {
-                try
-                {
-                    if (_dictionary.TryGetValue(key, out node))
-                    {
-                        node.Visited = true;
-                        return node.Value;
-                    }
-                    var value = await factory(key);
-                    node = new Node(key, value);
-                    lock (_dictionary)
-                    {
-                        if (_dictionary.Count == capacity) Evict();
-                        AddToHead(node);
-                        _dictionary[key] = node;
-                    }
-                    return value;
-                }
-                finally
-                {
-                    tcs.SetResult();
-                    _taskCompletionSources.TryRemove(key, out _);
-                }
+                if (_dictionary.Count == capacity) Evict();
+                AddToHead(node);
+                _dictionary[key] = node;
             }
-            else
-            {
-                await tcs.Task;
-            }
+            tcs.SetResult(value);
+            return value;
+        }
+        catch (Exception ex)
+        {
+            tcs.SetException(ex);
+            throw;
+        }
+        finally
+        {
+            _taskCompletionSources.TryRemove(key, out _);
         }
     }
+
+    public IEnumerator<KeyValuePair<K, V>> GetEnumerator() => _dictionary.Select(kv => KeyValuePair.Create(kv.Key, kv.Value.Value)).GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public int Count => _dictionary.Count;
 }
---
+++ Tests/SeiveLruCacheTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Tests;
+
+using CsCheck;
+using System.Collections.Concurrent;
+using Optimized.Collections;
+using Xunit;
+
+public class SieveLruCacheTests
+{
+    [Fact]
+    public async Task SieveExample()
+    {
+        var cache = new SieveLruCache<char, int>(7);
+        var i = 0;
+        var usedFactory = (char _) => Task.FromResult(i++);
+        var notUsedFactory = Task<int> (char _) => throw new Exception();
+
+        // set up initial state
+        await cache.GetAsync('A', usedFactory);
+        await cache.GetAsync('B', usedFactory);
+        await cache.GetAsync('C', usedFactory);
+        await cache.GetAsync('D', usedFactory);
+        await cache.GetAsync('B', notUsedFactory);
+        await cache.GetAsync('E', usedFactory);
+        await cache.GetAsync('F', usedFactory);
+        await cache.GetAsync('G', usedFactory);
+        await cache.GetAsync('A', notUsedFactory);
+        await cache.GetAsync('G', notUsedFactory);
+
+        // requests
+        await cache.GetAsync('H', usedFactory);
+        await cache.GetAsync('A', notUsedFactory);
+        await cache.GetAsync('D', notUsedFactory);
+        await cache.GetAsync('I', usedFactory);
+        await cache.GetAsync('B', notUsedFactory);
+        await cache.GetAsync('J', usedFactory);
+
+        (char, int)[] expected = [
+            ('A', 0),
+            ('B', 1),
+            ('D', 3),
+            ('G', 6),
+            ('H', 7),
+            ('I', 8),
+            ('J', 9),
+        ];
+
+        Assert.Equal(expected, cache.Select(kv => (kv.Key, kv.Value)).Order(), EqualityComparer<(char, int)>.Default);
+    }
+
+    [Fact]
+    public void TestConcurrent()
+    {
+        Check.SampleConcurrent(
+            Gen.Const(() => new SieveLruCache<int, int>(4)),
+            Gen.Int[1, 5].Operation<SieveLruCache<int, int>>((d, i) => d.GetAsync(i, j => Task.FromResult(j)).Wait()),
+            equal: (d1, d2) => d1.Count == d2.Count
+            );
+    }
+}
